7709aa1c1b3da15c92f5f058621f819edf1f8d03
alexcarrera-oss <125099124+alexcarrera-oss@users.noreply.github.com>
2023-02-11 01:30:14 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+import getDataSim
+import getTime
+
+import random
+import time
+import datetime
+
+now = getTime.now()
+
+
+  
+
+
+
+while(True):
+    print(getDataSim.message(now))
+    time.sleep(.1)
+
